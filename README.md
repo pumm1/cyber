49fd32a32f1eedfcf06b451ae62a44ebc4887f22
pumm1 <pumm1@gmail.com>
2022-12-20 14:19:47 +0200

    Update db init and README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# cyber
+# Cyberpunk 2020 Referee helper tool 
+
+## In progress
+
+## Features:
+
+- Stun checks based on damage
+- Roll stun effect
+- Damage calculation based on body type modifer
+- Determine hit locations
+- Fetch character info from DB
--- db/init.sql
+++ db/init.sql
@@ -1,2 +1,7 @@
+create user cyber with password 'cyber1';
+
+create schema cyberpunk authorization cyber;
+
 grant select on all tables in schema cyberpunk to cyber;
-grant select on all tables in schema cyberpunk to cyber;
+grant USAGE on SCHEMA cyberpunk to cyber ;
+grant select, insert, update, delete on all tables in schema cyberpunk to cyber;
