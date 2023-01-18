ef9036bf65e7b3a93968cf2d80cce229826e03bc
pumm1 <pumm1@gmail.com>
2023-01-18 20:16:35 +0200

    Update heal calculation logic

--- src/healing.py
+++ src/healing.py
@@ -80,18 +80,41 @@ def medicalCheck(name, given_roll):
 
 #TODO: add other bonuses, e.g. nanomachines
 def calculateHealingAmount(days):
-    print(f'Include medtech bonus? {yes_no}')
+    print(f'Include medtech bonus (+1)? {yes_no}')
     healing_rate = 1
     heal_days = safeCastToInt(days)
+    i = askInput()
+    med_tech_bonus = 0
+    while True:
+        if i == 'y':
+            healing_rate += 1
+            printGreenLine('+1 To healing rate')
+            med_tech_bonus = heal_days
+            break
+        elif i == 'n':
+            break
+    print(f'Use nano machines? (+1) {yes_no}')
+    give_nano_bonus = False
     i = askInput()
     while True:
         if i == 'y':
             healing_rate += 1
+            printGreenLine('+1 To healing rate')
+            give_nano_bonus = True
             break
         elif i == 'n':
             break
-    healing = heal_days * healing_rate
-    printGreenLine(f'{healing}HP recovered in {heal_days} days')
+    print(f"Use speedheal? (+1/day) {coloredText(Fore.GREEN, 'Give doses')}")
+    i = askInput()
+    speed_doses = safeCastToInt(i)
+    if speed_doses > heal_days:
+        speed_doses = heal_days
+    healing = heal_days * healing_rate + speed_doses
+    nano_bonus = 0
+    if give_nano_bonus:
+        nano_bonus = heal_days
+    printGreenLine(f'{healing}HP recovered in {heal_days} days \n'
+                   f'[days healed = {heal_days}, nano machine bonus = {nano_bonus}, speedheal bonus = {speed_doses}, med tech bonus = {med_tech_bonus}]')
 
 
 def healCharacter(name, amount):
