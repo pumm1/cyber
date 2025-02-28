e4de8381bdbf27574d2217e07d848d9403a9e5fb
pumm1 <patsu01@gmail.com>
2025-02-28 18:42:31 +0200

    Tweak UI

--- ui/src/GrenadeTable.tsx
+++ ui/src/GrenadeTable.tsx
@@ -1,10 +1,55 @@
+import { useState } from 'react'
 import Dice from './Dice'
 import './GrenadeTable.css'
 import Hideable from './Hideable'
 
+
+type Grid = number | string
+
+const topVal: Grid = '8,10'
+const topLeftVal: Grid = 7
+const topRightVal: Grid = 9
+const targetVal: Grid = 'Target'
+const leftVal: Grid = 5
+const rightVal: Grid = 6
+const bottomVal = '3,1'
+const bottomLeftVal = 2
+const bottomRightVal = 4
+
+const matchDirectionToGrid = (dirNum: number): Grid => {
+    switch(dirNum) {
+        case 1:
+        case 3:
+            return bottomVal
+        case 2:
+            return bottomLeftVal
+        case 4:
+            return bottomRightVal
+        case 5:
+            return leftVal
+        case 6:
+            return rightVal
+        case 7:
+            return topLeftVal
+        case 8:
+        case 10:
+            return topVal
+        case 9:
+            return topRightVal
+        default:
+            return targetVal
+    }
+}
+
+const gridMatches = (grid: Grid, directionToUse?: number): boolean => {
+    const directionToUseGrid = directionToUse ? matchDirectionToGrid(directionToUse) === grid : false
+
+    return directionToUseGrid
+}
+
 const GrenadeTable = ({}) => {
-    type Grid = number | string
-    const grids: Grid[]  = [7, '8 - 10', 9, 5, 'Target', 6, 2, '3 - 1', 4]
+    const grids: Grid[]  = [topLeftVal, topVal, topRightVal, leftVal, targetVal, rightVal, bottomLeftVal, bottomVal, bottomRightVal]
+    const [directionVal, setDirectionVal] = useState<number | undefined>()
 
     const grenadeTableProp = 
         <>
@@ -12,7 +57,7 @@ const GrenadeTable = ({}) => {
             <div className='missContainer'>
                 <span className='miss'>
                     Direction
-                    <Dice numberOfDice={1} dDie={10}></Dice>
+                    <Dice numberOfDice={1} dDie={10} updateResult={setDirectionVal}></Dice>
                 </span>
                 <span className='miss'>
                     Distance
@@ -20,7 +65,7 @@ const GrenadeTable = ({}) => {
                 </span>
                 <div className='grenadeTable'>
                     <div className='tableContainer'>
-                            {grids.map(g => <span key={g}>{g}</span>)}
+                            {grids.map(g => <span className={gridMatches(g, directionVal) ? 'gridDirection' : undefined} key={g}>{g}</span>)}
                     </div>
                 </div>
             </div>
